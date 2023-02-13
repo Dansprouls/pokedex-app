608119f0b6f7bd61070e5c9168ceb0d7fdfd6fc2
Dansprouls <dspr1015@gmail.com>
2023-02-13 11:37:23 -0500

    Added an array of Pokemon Objects
    Keys for each of the 14 Pokemon Objects include pokemonName, pokemonHeight, and pokemonType.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('Hello world');
-
-let favoriteFood = 'Chicken Parm';
-document.write(favoriteFood);
+let pokemonList = [
+  {pokemonName: 'Articuno', pokemonHeight: 1.7, pokemonType: ['Ice', 'Flying']},
+  {pokemonName: 'Zapdos', pokemonHeight: 1.6, pokemonType: ['Electric', 'Flying']},
+  {pokemonName: 'Moltres', pokemonHeight: 2, pokemonType: ['Fire', 'Flying']},
+  {pokemonName: 'Bulbasaur', pokemonHeight: 0.7, pokemonType: ['Grass', 'Poison']},
+  {pokemonName: 'Ivysaur', pokemonHeight: 1, pokemonType: ['Grass', 'Poison']},
+  {pokemonName: 'Venusaur', pokemonHeight: 2, pokemonType: ['Grass', 'Poison']},
+  {pokemonName: 'Charmander', pokemonHeight: 0.6, pokemonType: 'Fire'},
+  {pokemonName: 'Charmeleon', pokemonHeight: 1.1, pokemonType: 'Fire'},
+  {pokemonName: 'Charizard', pokemonHeight: 1.7, pokemonType: ['Fire', 'Flying']},
+  {pokemonName: 'Squirtle', pokemonHeight: 0.5, pokemonType: 'Water'},
+  {pokemonName: 'Wartortle', pokemonHeight: 1, pokemonType: 'Water'},
+  {pokemonName: 'Blastoise', pokemonHeight: 1.6, pokemonType: 'Water'},
+  {pokemonName: 'Pikachu', pokemonHeight: 0.4, pokemonType: 'Electric'},
+  {pokemonName: 'Raichu', pokemonHeight: 0.8, pokemonType: 'Electric'}
+]
