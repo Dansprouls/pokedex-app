b9a3d6d15e9a3222c9967fed3fc216027313b981
Dansprouls <dspr1015@gmail.com>
2023-03-09 13:44:49 -0500

    Revert "Merge branch 'main' of https://github.com/Dansprouls/pokedex-app"
    This reverts commit 06e1fa853ad5fd7b802c51a2c43c2525360997b4, reversing
    changes made to 0d1e4759ecd4ba608a49fde2ab1b819e3ae69ac4.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //IIFE to manage scope
 let pokemonRepository = (function(){ 
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1200';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#pokemon-modal-container');
 
   //search for pokemon name in search bar
   let searchButton = $('.btn-warning');
@@ -47,6 +48,21 @@ let pokemonRepository = (function(){
 
   }
 
+  /*
+  function addListItem(pokemon) {
+    let pokedexList = document.querySelector('.pokemon-list'); //pokedexList = UL in html
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-name-list');
+    listItem.appendChild(button);
+    listItem.classList.add('group-list-item'); //added in
+    pokedexList.appendChild(listItem);
+    button.addEventListener('click', function(event){
+      showDetails(pokemon); //what is the event param?
+    });
+  }*/
+
   //loads list of pokemon from API - uses JSON to communicate with API
   //may need some walkthrough on this section
   function loadList() {
@@ -84,6 +100,7 @@ let pokemonRepository = (function(){
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
       showPokemonModal(item);
+
     })
   }
 
@@ -99,6 +116,68 @@ let pokemonRepository = (function(){
     modalBody.append(`<p>Height: ${pokemon.height}</p>`);
     modalBody.append(`<p>Types: ${pokemon.types}</p>`);
   }
+
+  //NEEDS TO BE UPDATED FOR BOOTSTRAP
+  /*
+  function showPokemonModal(pokemon){
+    const { name, height, types, imageUrl } = pokemon;
+    modalContainer.innerHTML = '';
+    let pokemonModal = document.createElement('div');
+    pokemonModal.classList.add('pokemon-modal');
+
+    let closeButton = document.createElement('button');
+    closeButton.classList.add('modal-close');
+    closeButton.innerText = 'X';
+    closeButton.addEventListener('click', hideModal);
+
+    let modalTitle = document.createElement('h1');
+    modalTitle.classList.add('modal-content', 'modal-title');
+    modalTitle.innerText = name;
+
+    let modalHeight = document.createElement('p');
+    modalHeight.classList.add('modal-content', 'modal-list-item');
+    //modalHeight.innerText = height;
+    modalHeight.innerText = 'Height: ' + height;
+
+    let modalTypes = document.createElement('p');
+    modalTypes.classList.add('modal-content', 'modal-list-item');
+    modalTypes.innerText = 'Type(s): ' + types;
+
+    let modalImg = document.createElement('img');
+    modalImg.classList.add('modal-content', 'modal-img');
+    modalImg.src = imageUrl;
+
+    pokemonModal.appendChild(closeButton);
+    pokemonModal.appendChild(modalTitle);
+    pokemonModal.appendChild(modalHeight);
+    pokemonModal.appendChild(modalTypes);
+    pokemonModal.appendChild(modalImg);
+    modalContainer.appendChild(pokemonModal);
+
+
+
+    modalContainer.classList.add('is-visible');
+    } */
+
+    /*
+    function hideModal() {
+      modalContainer.classList.remove('is-visible');
+    }
+    */
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();
+      }
+    });
+
+    
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+    
   
   return {
     add: add,
@@ -108,7 +187,7 @@ let pokemonRepository = (function(){
     loadList: loadList,
     loadDetails: loadDetails,
     showDetails: showDetails,
-    showPokemonModal: showPokemonModal 
+    showPokemonModal: showPokemonModal //do i need?
   };
 })();
 
